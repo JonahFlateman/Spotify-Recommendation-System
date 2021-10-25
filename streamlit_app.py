f51ff433f3a5cf1d78e11f04377e88ca4fd6c116
Jonah Flateman <jonahflateman@gmail.com>
2021-10-25 09:47:41 -0400

    changed app name

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,8 +13,8 @@ from collections import defaultdict
 from scipy.spatial.distance import cdist
 import time
 
-cid = 'YOUR CLIENT ID'
-secret = 'YOUR CLIENT SECRET'
+#cid = 'YOUR CLIENT ID'
+#secret = 'YOUR CLIENT SECRET'
 client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
 sp = spotipy.Spotify(client_credentials_manager
 =
